12538eb3ea954e27c87e0134501bcbc8898b10be
Nenad Živanović <nen.zivanovic@gmail.com>
2017-07-07 13:16:17 +0200

    Add ability to disable and re-enable Laravel's exception handling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, InteractsWithExceptionHandling;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->withoutExceptionHandling();
+    }
 }
