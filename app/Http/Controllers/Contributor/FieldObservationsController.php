1f61d888344bf1fafb1a716f089499f22237d00b
Nenad Živanović <nen.zivanovic@gmail.com>
2017-08-03 23:38:39 +0200

    Refactor storing new field observation to a form request

--- app/Http/Controllers/Contributor/FieldObservationsController.php
+++ app/Http/Controllers/Contributor/FieldObservationsController.php
@@ -6,6 +6,7 @@ use App\Comment;
 use App\FieldObservation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Forms\NewFieldObservationForm;
 
 class FieldObservationsController extends Controller
 {
@@ -19,50 +20,10 @@ class FieldObservationsController extends Controller
      *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(NewFieldObservationForm $form)
     {
-        $this->validate($request, [
-            'year' => 'required|date_format:Y|before_or_equal:now',
-            'latitude' => 'required|numeric|between:-90,90',
-            'longitude'=> 'required|numeric|between:-180,180',
-            'altitude'=> 'required|numeric',
-            'accuracy' => 'required|numeric',
-            'source' => 'nullable|string',
-            'photos' => 'nullable|array|max:'.config('alciphron.photos_per_observation'),
-        ]);
-
-        $observation = $this->createObservation($request->all());
-
-        if ($comment = trim($request->input('comment'))) {
-            $observation->addNewComment($comment);
-        }
-
-        $observation->details->addPhotos($request->input('photos', []));
+        $form->save();
 
         return redirect('/contributor/field-observations');
-    }
-
-    /**
-     * Create observation.
-     *
-     * @param  array  $data
-     * @return \App\Observation
-     */
-    protected function createObservation($data)
-    {
-        return FieldObservation::create([
-            'source' => $data['source'],
-        ])->observation()->create([
-            'year' => $data['year'],
-            'month' => $data['month'],
-            'day' => $data['day'],
-            'location' => $data['location'],
-            'latitude' => $data['latitude'],
-            'longitude' => $data['longitude'],
-            'mgrs10k' => mgrs10k($data['latitude'], $data['longitude']),
-            'accuracy' => $data['accuracy'],
-            'altitude' => $data['altitude'],
-            'created_by_id' => auth()->user()->id,
-        ]);
     }
 }
---
+++ app/Http/Forms/NewFieldObservationForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Forms;
+
+use App\FieldObservation;
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewFieldObservationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'year' => 'required|date_format:Y|before_or_equal:now',
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude'=> 'required|numeric|between:-180,180',
+            'altitude'=> 'required|numeric',
+            'accuracy' => 'required|numeric',
+            'source' => 'nullable|string',
+            'photos' => 'nullable|array|max:'.config('alciphron.photos_per_observation'),
+        ];
+    }
+
+    /**
+     * Store observation and related data.
+     *
+     * @return \App\Observation
+     */
+    public function save()
+    {
+        $observation = $this->createObservation($this->all());
+
+        if ($comment = trim($this->input('comment'))) {
+            $observation->addNewComment($comment);
+        }
+
+        $observation->details->addPhotos($this->input('photos', []));
+
+        return $observation;
+    }
+
+    /**
+     * Create observation.
+     *
+     * @param  array  $data
+     * @return \App\Observation
+     */
+    protected function createObservation($data)
+    {
+        return FieldObservation::create([
+            'source' => $data['source'],
+        ])->observation()->create([
+            'year' => $data['year'],
+            'month' => $data['month'],
+            'day' => $data['day'],
+            'location' => $data['location'],
+            'latitude' => $data['latitude'],
+            'longitude' => $data['longitude'],
+            'mgrs10k' => mgrs10k($data['latitude'], $data['longitude']),
+            'accuracy' => $data['accuracy'],
+            'altitude' => $data['altitude'],
+            'created_by_id' => auth()->user()->id,
+        ]);
+    }
+}
