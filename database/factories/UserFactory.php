2148a7cdade022980f401c57e5d5383d90376f6e
Nenad Živanović <nen.zivanovic@gmail.com>
2017-12-20 22:11:17 +0100

    Use fixed password hash instead of generating it every time

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,13 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
-    static $password;
-
     return [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
         'remember_token' => str_random(10),
         'settings' => '[]',
         'verified' => true,
