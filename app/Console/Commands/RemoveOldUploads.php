5a10926cf7ca220cd691e4c8f72384b2ec9d2a62
Nenad Živanović <nen.zivanovic@gmail.com>
2018-03-15 14:52:05 +0100

    Cleaning up old uploads

---
+++ app/Console/Commands/RemoveOldUploads.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class RemoveOldUploads extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'uploads:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove old uploads to free up disk space.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->warn('Removing old uploaded files...');
+
+        Storage::disk('public')->delete(array_filter(
+            Storage::disk('public')->allFiles('uploads'),
+            [$this, 'fileShouldBeRemoved']
+        ));
+
+        $this->info('Old uploaded files have been removed!');
+    }
+
+    /**
+     * Check if file should be removed.
+     *
+     * @param  string  $file
+     * @return bool
+     */
+    protected function fileShouldBeRemoved($file)
+    {
+        return Storage::disk('public')->lastModified($file) < Carbon::yesterday()->getTimestamp();
+    }
+}
---
+++ tests/Feature/CleaningOldUploads.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Carbon;
+
+use Illuminate\Http\Testing\File;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CleaningOldUploads extends TestCase
+{
+    /** @test */
+    public function can_remove_old_uploaded_files()
+    {
+        Storage::fake('public');
+        $path = Storage::disk('public')->put('uploads', File::image('test.jpg'));
+
+        Storage::disk('public')->assertExists($path);
+
+        Carbon::setTestNow(Carbon::tomorrow()->addDay());
+        $this->artisan('uploads:clean');
+
+        Storage::disk('public')->assertMissing($path);
+    }
+}
