d50eeed5894c9b12f6e02f35bb5c5b1394e30b69
Nenad Živanović <nen.zivanovic@gmail.com>
2018-03-26 19:58:56 +0200

    Build sidebar menu using Spatie Menu package to easily show active link

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Spatie\Menu\Laravel\Facades\Menu;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
@@ -20,6 +21,48 @@ class ViewServiceProvider extends ServiceProvider
 
         Blade::if('roles', function ($roles) {
             return auth()->user()->hasAllRole(array_wrap($roles));
+        });
+
+        $this->buildSidebarMenu();
+    }
+
+    /**
+     * Build sidebar menu using Spatie Menu.
+     *
+     * @return \Spatie\Menu\Laravel\Menu
+     */
+    protected function buildSidebarMenu()
+    {
+        return Menu::macro('sidebar', function () {
+            return Menu::new()
+                ->setWrapperTag('aside')
+                ->withoutParentTag()
+                ->addClass('menu')
+                ->add(Menu::new()
+                    ->prepend('<p class="menu-label">'.__('navigation.my').'</p>')
+                    ->addClass('menu-list')
+                    ->route('contributor.field-observations.index', __('navigation.field_observations'))
+                    ->setActiveClass('is-active')
+                    ->setActiveClassOnLink()
+                    ->setActiveFromRequest()
+                )->addIf(auth()->user()->hasAnyRole(['admin', 'curator']), Menu::new()
+                    ->prepend('<p class="menu-label">'.__('navigation.curator').'</p>')
+                    ->addClass('menu-list')
+                    ->routeIfCan(['list', \App\FieldObservation::class], 'curator.pending-observations.index', __('navigation.pending_observations'))
+                    ->routeIfCan(['list', \App\FieldObservation::class], 'curator.approved-observations.index', __('navigation.approved_observations'))
+                    ->routeIfCan(['list', \App\FieldObservation::class], 'curator.unidentifiable-observations.index', __('navigation.unidentifiable_observations'))
+                    ->setActiveClass('is-active')
+                    ->setActiveClassOnLink()
+                    ->setActiveFromRequest()
+                )->addIf(auth()->user()->hasAnyRole(['admin', 'curator']), Menu::new()
+                    ->prepend('<p class="menu-label">'.__('navigation.admin').'</p>')
+                    ->addClass('menu-list')
+                    ->routeIfCan(['list', \App\Taxon::class], 'admin.taxa.index', __('navigation.taxa'))
+                    ->routeIfCan(['list', \App\User::class], 'admin.users.index', __('navigation.users'))
+                    ->setActiveClass('is-active')
+                    ->setActiveClassOnLink()
+                    ->setActiveFromRequest()
+                );
         });
     }
 
