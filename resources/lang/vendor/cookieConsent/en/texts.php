595cea1a7e0f38d06159de2282845feba6d9a1ff
Nenad Živanović <nen.zivanovic@gmail.com>
2018-05-02 14:36:41 +0200

    Add cookie consent dialog

---
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'This site uses cookies in order to keep track of your login session.',
+    'agree' => 'Ok',
+];
---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,13 @@
+<div class="js-cookie-consent cookie-consent">
+    <div class="columns">
+        <div class="column cookie-consent__message">
+            {!! trans('cookieConsent::texts.message') !!}
+        </div>
+
+        <div class="column is-narrow cookie-consent__agree">
+            <button class="js-cookie-consent-agree button is-secondary">
+                {{ trans('cookieConsent::texts.agree') }}
+            </button>
+        </div>
+    </div>
+</div>
