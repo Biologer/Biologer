7971fee40c9f0a46a08189adef8c7095431a74bf
Nenad Živanović <nen.zivanovic@gmail.com>
2018-05-03 18:38:27 +0200

    Update sponsors page

--- resources/views/sr/pages/sponsors.blade.php
+++ resources/views/sr/pages/sponsors.blade.php
@@ -4,6 +4,6 @@
     <section class="section has-text-centered min-h-screen">
         <img src="https://www.rufford.org/sites/all/themes/rufford/img/rufford.jpg" alt="Rufford" class="image mx-auto">
 
-        Развој апликације финансиран средствима "Rufford Small Grants" пројекта бр. 20507-B
+        Развој апликације финансиран средствима "Rufford Small Grants" пројекта бр. <a href="http://www.rufford.org/projects/ana_golubovi%C4%87_0">20507-B</a> и <a href="https://www.rufford.org/projects/milo%C5%A1_popovi%C4%87_0">24652-B</a>
     </section>
 @endsection
