e1b56d0ed040797e8122fb9354338c83cbd92586
Nenad Živanović <nen.zivanovic@gmail.com>
2018-09-06 23:44:54 +0200

    Allow model filters to be be overriden

--- app/Filters/Filterable.php
+++ app/Filters/Filterable.php
@@ -9,14 +9,16 @@ trait Filterable
         return [];
     }
 
-    public function scopeFilter($query, $request)
+    public function scopeFilter($query, $request, array $filters = [])
     {
-        $filters = $this->filters();
+        $filters = $filters ?: $this->filters();
 
         foreach ($request->all() as $param => $value) {
-            if (array_key_exists($param, $filters)
-                && $value && class_exists($filters[$param])) {
-                $query = (new $filters[$param]())->apply($query, $value, $param);
+            $shouldFilter = array_key_exists($param, $filters)
+                && $value && class_exists($filters[$param]);
+
+            if ($shouldFilter) {
+                $query = (new $filters[$param]())->apply($query, $value, $param, $request);
             }
         }
 
