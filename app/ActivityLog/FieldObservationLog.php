3883d3f504642822327c2f3e624cad9f57e8b4f1
Nenad Živanović <nen.zivanovic@gmail.com>
2018-10-20 17:18:57 +0200

    Fix logging license change when changing from inactive license

--- app/ActivityLog/FieldObservationLog.php
+++ app/ActivityLog/FieldObservationLog.php
@@ -58,7 +58,7 @@ class FieldObservationLog
                     // We need it with the key of "data_license", not "license".
                     $data['data_license'] = [
                         'value' => $value,
-                        'label' => 'licenses.'.License::findById($value)->name,
+                        'label' => ($license = License::findById($value)) ? 'licenses.'.$license->name : null,
                     ];
                 } elseif ('found_dead' === $key) {
                     $data[$key] = ['value' => $value, 'label' => $value ? 'Yes' : 'No'];
