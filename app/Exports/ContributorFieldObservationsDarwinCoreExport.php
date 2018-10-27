41ac3b809819e4496f23e01ba47773e11eac9994
Nenad Živanović <nen.zivanovic@gmail.com>
2018-10-27 11:41:44 +0200

    Implement Darwin Core export for field observations

---
+++ app/Exports/ContributorFieldObservationsDarwinCoreExport.php
@@ -0,0 +1,303 @@
+<?php
+
+namespace App\Exports;
+
+use App\Taxon;
+use App\Export;
+use App\FieldObservation;
+
+class ContributorFieldObservationsDarwinCoreExport extends BaseExport
+{
+    public static function createFiltered($filters = [])
+    {
+        return static::create(static::availableColumns()->all(), $filters, true);
+    }
+
+    /**
+     * Column labels and names.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function columnData()
+    {
+        return collect([
+            'taxonID',
+            'kingdom',
+            'phylum',
+            'class',
+            'order',
+            'family',
+            'genus',
+            'specificEpithet',
+            'species',
+            'scientificNameAuthorship',
+            'infraspecificEpithet',
+            'scientificName',
+            'acceptedNameUsage',
+            'previousIdentifications',
+            'taxonRank',
+            'vernacularName',
+            'vernacularNameSerbian',
+            'vernacularNameSerbian_latin',
+            'vernacularNameCroatian',
+            'taxonomicStatus',
+            'identifiedBy',
+            'dateIdentified',
+            'basisOfRecord',
+            'dcterms:type',
+            'typeOfRecord',
+            'dcterms:rightsHolder',
+            'dcterms:accessRights',
+            'dcterms:license',
+            'minimumElevationInMeters',
+            'maximumElevationInMeters',
+            'recordNumber',
+            'verbatimLocality',
+            'verbatimEventDate',
+            'verbatimElevation',
+            'verbatimCoordinates',
+            'sex',
+            'lifeStage',
+            'associatedMedia',
+            'locality',
+            'geodeticDatum',
+            'decimalLatitude',
+            'decimalLongitude',
+            'coordinateUncertaintyInMeters',
+            'georeferencedBy',
+            'georeferencedDate',
+            'georeferenceProtocol',
+            'individualCount',
+            'recordedBy',
+            'eventDate',
+            'eventTime',
+            'year',
+            'month',
+            'day',
+
+            'ReferenceID',
+            'ReferenceType',
+            'ReferenceAuthors',
+            'ReferenceEditors',
+            'ReferenceYear',
+            'ReferencePage',
+            'ReferencePages',
+            'ReferenceTitle',
+            'ReferencePublicationTitle',
+            'ReferencePublisher',
+            'ReferencePlace',
+            'ReferenceDOI',
+            'ReferenceLink',
+            'ReferencePdf',
+            'Reference',
+        ])->map(function ($column) {
+            return [
+                'label' => $column,
+                'value' => $column,
+            ];
+        });
+    }
+
+    /**
+     * Database query to get the data for export.
+     *
+     * @param  \App\Export  $export
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+     */
+    protected function query(Export $export)
+    {
+        return FieldObservation::with([
+            'observation.taxon.ancestors', 'observation.photos', 'observedBy',
+            'identifiedBy', 'observation.types.translations', 'observation.stage',
+            'activity'
+        ])->filter($export->filter)->orderBy('id')->createdBy($export->user);
+    }
+
+    /**
+     * Extract needed data from item.
+     *
+     * @param  mixed  $item
+     * @return array
+     */
+    protected function transformItem($item)
+    {
+        $taxon = $item->observation->taxon;
+
+        return [
+            'taxonID' => $item->observation->taxon_id,
+            'kingdom' => $this->getTaxonOfRank($taxon, 'kingdom'),
+            'phylum' => $this->getTaxonOfRank($taxon, 'phylum'),
+            'class' => $this->getTaxonOfRank($taxon, 'class'),
+            'order' => $this->getTaxonOfRank($taxon, 'order'),
+            'family' => $this->getTaxonOfRank($taxon, 'family'),
+            'genus' => $this->getTaxonOfRank($taxon, 'genus'),
+            'specificEpithet' => $this->extractSpeciesEpithet($taxon),
+            'species' => $this->getTaxonOfRank($taxon, 'species'),
+            'scientificNameAuthorship' => optional($taxon)->author,
+            'infraspecificEpithet' => $this->extractInfraspeciesEpithet($taxon),
+            'scientificName' => $this->firstIdentification($item),
+            'acceptedNameUsage' => implode(' ', array_filter([optional($taxon)->name, optional($taxon)->author])),
+            'previousIdentifications' => '', // TODO: Get previous identifications
+            'taxonRank' => optional($taxon)->rank,
+            'vernacularName' => $this->getTaxonNativeName($taxon, 'en'),
+            'vernacularNameSerbian' => $this->getTaxonNativeName($taxon, 'sr'),
+            'vernacularNameSerbian_latin' => $this->getTaxonNativeName($taxon, 'sr-Latn'),
+            'vernacularNameCroatian' => $this->getTaxonNativeName($taxon, 'hr'),
+            'taxonomicStatus' => 'valid',
+            'identifiedBy' => $item->observation->identifier,
+            'dateIdentified' => optional($this->identifiedAt($item))->toIso8601String(),
+            'basisOfRecord' => 'HumanObservation',
+            'dcterms:type' => $item->observation->photos->isNotEmpty() ? 'StillImage' : 'Event',
+            'typeOfRecord' => $item->observation->types->map(function ($type) {
+                return $type->translations->where('locale', 'en')->first();
+            })->pluck('name')->filter()->implode('|'),
+            'dcterms:rightsHolder' => config('app.name'),
+            'dcterms:accessRights' => optional($item->license())->name,
+            'dcterms:license' => optional($item->license())->link,
+            'minimumElevationInMeters' => $item->observation->elevation,
+            'maximumElevationInMeters' => $item->observation->elevation,
+            'recordNumber' => $item->observation->id,
+            'verbatimLocality' => '',
+            'verbatimEventDate' => '',
+            'verbatimElevation' => '',
+            'verbatimCoordinates' => '',
+            'sex' => $item->observation->sex,
+            'lifeStage' => optional($item->observation->stage)->name,
+            'associatedMedia' => $item->observation->photos->pluck('public_url')->filter()->implode('|'),
+            'locality' => $item->observation->location,
+            'geodeticDatum' => 'WGS84',
+            'decimalLatitude' => $item->observation->latitude,
+            'decimalLongitude' => $item->observation->longitude,
+            'coordinateUncertaintyInMeters' => $item->observation->accuracy,
+            'georeferencedBy' => $item->observation->observer,
+            'georeferencedDate' => $item->observation->created_at->toIso8601String(),
+            'georeferenceProtocol' => '',
+            'individualCount' => $item->observation->number,
+            'recordedBy' => $item->observation->observer,
+            'eventDate' => '',
+            'eventTime' => optional($item->time)->format('H:i'),
+            'year' => $item->observation->year,
+            'month' => $item->observation->month,
+            'day' => $item->observation->day,
+
+            'ReferenceID' => '',
+            'ReferenceType' => '',
+            'ReferenceAuthors' => '',
+            'ReferenceEditors' => '',
+            'ReferenceYear' => '',
+            'ReferencePage' => '',
+            'ReferencePages' => '',
+            'ReferenceTitle' => '',
+            'ReferencePublicationTitle' => '',
+            'ReferencePublisher' => '',
+            'ReferencePlace' => '',
+            'ReferenceDOI' => '',
+            'ReferenceLink' => '',
+            'ReferencePdf' => '',
+            'Reference' => '',
+        ];
+    }
+
+    /**
+     * Get name of anacestor of given rank.
+     *
+     * @param  \App\Taxon|null  $taxon
+     * @param  string  $rank
+     * @return string
+     */
+    private function getTaxonOfRank($taxon, $rank)
+    {
+        if (! $taxon || $taxon->rank_level > Taxon::RANKS[$rank]) {
+            return '';
+        }
+
+        if ($taxon->rank === $rank) {
+            return $taxon->name;
+        }
+
+        $rankTaxon = $taxon->ancestors->where('rank', $rank)->first();
+
+        return $rankTaxon ? $rankTaxon->name : '';
+    }
+
+    /**
+     * Extract infraspecies epithet.
+     *
+     * @param  \App\Taxon|null  $taxon
+     * @return string
+     */
+    private function extractInfraspeciesEpithet($taxon)
+    {
+        if (!$taxon || $taxon->rank_level >= Taxon::RANKS['species']) {
+            return '';
+        }
+
+        $value = explode(' ', $taxon->name);
+
+        unset($value[0], $value[1]);
+
+        return implode(' ', $value);
+    }
+
+    /**
+     * Extract species epithet.
+     *
+     * @param  \App\Taxon|null  $taxon
+     * @return string
+     */
+    private function extractSpeciesEpithet($taxon)
+    {
+        if (!$taxon || $taxon->rank_level >= Taxon::RANKS['genus']) {
+            return '';
+        }
+
+        $value = explode(' ', $taxon->name);
+
+        return $value[1] ?? '';
+    }
+
+    /**
+     * Get name of the first identification.
+     *
+     * @param  \App\FieldObservation  $fieldObservation
+     * @return string
+     */
+    private function firstIdentification($fieldObservation)
+    {
+        $taxon = $fieldObservation->observation->taxon;
+
+        $activity = $fieldObservation->activity->last(function ($activity) {
+            return $activity->changes()->collect('old')->has('taxon');
+        });
+
+        if ($activity) {
+            return $activity->changes()->collect('old')->get('taxon');
+        }
+
+        return $taxon ? $taxon->name : '';
+    }
+
+    private function identifiedAt($fieldObservation)
+    {
+        if (! $fieldObservation->observation->taxon) {
+            return;
+        }
+
+        $activity = $fieldObservation->activity->first(function ($activity) {
+            return $activity->changes()->collect('old')->has('taxon');
+        });
+
+        if ($activity) {
+            return $activity->created_at;
+        }
+
+        return $fieldObservation->created_at;
+    }
+
+    private function getTaxonNativeName($taxon, $locale)
+    {
+        $translation = optional($taxon)->translateOrNew($locale);
+
+        return $translation ? $translation->name : '';
+    }
+}
--- app/Http/Controllers/Api/My/FieldObservationExportsController.php
+++ app/Http/Controllers/Api/My/FieldObservationExportsController.php
@@ -7,34 +7,76 @@ use App\Jobs\PerformExport;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use App\Exports\ContributorFieldObservationsExport;
+use App\Exports\ContributorFieldObservationsDarwinCoreExport;
 
 class FieldObservationExportsController extends Controller
 {
+    /**
+     * Start export of contributors field observations.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \App\Export
+     */
     public function store(Request $request)
     {
-        $request->validate([
-            'columns' => [
-                'required',
-                'array',
-                'min:1',
-                'distinct',
-                Rule::in(ContributorFieldObservationsExport::availableColumns()),
-            ],
-            'with_header' => ['boolean'],
+        $this->validateExport($request);
+
+        return tap($this->createExport($request), function ($export) {
+            PerformExport::dispatch($export);
+        });
+    }
+
+    private function validateExport($request)
+    {
+        $v = Validator::make($request->all(), [
+            'with_header' => ['nullable', 'boolean'],
+            'type' => ['required', 'string', 'in:darwin_core,custom']
         ], [], [
             'columns' => trans('labels.exports.columns'),
             'with_header' => trans('labels.exports.with_header'),
         ]);
 
-        $export = ContributorFieldObservationsExport::create(
+        $v->sometimes('columns', [
+            'required', 'array', 'min:1', 'distinct',
+            Rule::in(ContributorFieldObservationsExport::availableColumns())
+        ], function ($input) {
+            return $input->type === 'custom';
+        });
+
+        return $v->validate();
+    }
+
+    /**
+     * Create export instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \App\Export
+     */
+    private function createExport($request)
+    {
+        if ($request->input('type', 'custom') === 'darwin_core') {
+            return ContributorFieldObservationsDarwinCoreExport::createFiltered(
+                $this->getFiltersFromRequest($request)
+            );
+        }
+
+        return ContributorFieldObservationsExport::create(
             $request->input('columns'),
-            $request->only(array_keys(FieldObservation::filters())),
+            $this->getFiltersFromRequest($request),
             $request->input('with_header', false)
         );
+    }
 
-        PerformExport::dispatch($export);
-
-        return $export;
+    /**
+     * Extract filters from request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    private function getFiltersFromRequest($request)
+    {
+        return $request->only(array_keys(FieldObservation::filters()));
     }
 }
