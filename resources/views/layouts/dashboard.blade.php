70938a79a367aae4c29b4cc42cc86ee27d65c4f8
Nenad Živanović <nen.zivanovic@gmail.com>
2018-12-18 22:31:36 +0100

    Update laravel-mix to the latest version and split vendor dependencies

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -150,6 +150,8 @@
         @include('cookieConsent::index')
 
         @stack('beforeScripts')
+        <script src="{{ mix('js/manifest.js') }}"></script>
+        <script src="{{ mix('js/vendor.js') }}"></script>
         <script src="{{ mix('js/app.js') }}"></script>
         @stack('afterScripts')
     </body>
