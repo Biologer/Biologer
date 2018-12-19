d2016e6eef16c45459eb7cba0a736221ef83bce5
Nenad Živanović <nen.zivanovic@gmail.com>
2018-12-19 10:35:14 +0100

    Improve styling of announcement cards on the front page
    Move "Read more" link to the bottom for mobile screen sizes.

--- resources/views/partials/announcement.blade.php
+++ resources/views/partials/announcement.blade.php
@@ -1,11 +1,11 @@
-<article class="box media">
-    <div class="media-content">
+<article class="box flex flex-col tablet:flex-row">
+    <div class="flex-1">
         <div class="content">
             <h3 class="is-size-5">{{ $announcement->title }}</h3>
             <p>{{ $announcement->created_at->format('j.m.Y H:i') }} - {{ $announcement->authorName() }}</p>
         </div>
     </div>
-    <div class="media-right">
+    <div>
         <a href="{{ route('announcements.show', $announcement) }}">{{ __('pages.announcements.read') }}</a>
     </div>
 </article>
