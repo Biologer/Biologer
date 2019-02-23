300b5718db83de9b6d9656999a339f316b58b615
MBP <nen.zivanovic@gmail.com>
2019-02-23 10:36:33 +0100

    Update croatian translation of home page

--- resources/lang/hr/pages.php
+++ resources/lang/hr/pages.php
@@ -5,7 +5,7 @@ return [
         'browse' => 'Pregledaj podatke',
         'android_link' => 'Android aplikacija',
         'android_title' => 'Preuzmite Android aplikaciju',
-        'welcome' => 'Biologer je jednostavan i slobodan softver osmišljen za prikupljanje podataka o biološkoj raznovrsnosti.',
+        'welcome' => 'Biologer je jednostavan i slobodan softver osmišljen za prikupljanje podataka o biološkoj raznolikosti.',
 
         'announcements' => [
             'title' => 'Novosti',
