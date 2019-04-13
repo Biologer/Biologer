c2b48c3cf020b52be9918cb0d9a8ebfe3e1a615c
Nenad Živanović <nen.zivanovic@gmail.com>
2019-04-13 12:00:34 +0200

    Add description meta tag

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="description" content="{{ __('pages.home.welcome') }}">
+
         <link rel="canonical" href="{{ LaravelLocalization::getNonLocalizedURL() }}" />
 
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
