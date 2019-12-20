ad956ca166d587967b056b3c5b3cf6ca0be877e5
Nenad Živanović <nen.zivanovic@gmail.com>
2019-12-20 09:58:44 +0100

    We don't use sockets yet

--- app/Events/ImportStatusUpdated.php
+++ app/Events/ImportStatusUpdated.php
@@ -5,11 +5,10 @@ namespace App\Events;
 use App\Import;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ImportStatusUpdated implements ShouldBroadcast
+class ImportStatusUpdated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Events/NewAnnouncement.php
+++ app/Events/NewAnnouncement.php
@@ -5,11 +5,10 @@ namespace App\Events;
 use App\Announcement;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewAnnouncement implements ShouldBroadcast
+class NewAnnouncement
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
