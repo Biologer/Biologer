8a9d1d374f10ccdd3ab58835f47bc3e0105b2312
Nenad Živanović <nen.zivanovic@gmail.com>
2020-04-03 16:58:29 +0200

    Try to optimize

--- app/ViewGroup.php
+++ app/ViewGroup.php
@@ -160,11 +160,10 @@ class ViewGroup extends Model
         return $this->memoize('speciesIds', function () {
             return $this->species()->when($this->only_observed_taxa, function ($query) {
                 $query->leftJoin('taxon_ancestors', 'taxon_ancestors.ancestor_id', '=', 'taxa.id')
-                    ->addWhereExistsQuery(
-                        Observation::whereColumn('taxon_id', 'taxa.id')
-                            ->orWhereColumn('taxon_id', 'taxon_ancestors.model_id')
-                            ->getQuery()
-                    );
+                    ->where(function ($query) {
+                        $query->addWhereExistsQuery(Observation::whereColumn('taxon_id', 'taxa.id')->getQuery())
+                            ->addWhereExistsQuery(Observation::whereColumn('taxon_id', 'taxon_ancestors.model_id')->getQuery(), 'or');
+                    });
             })->selectRaw('DISTINCT(id) as id')->pluck('id');
         });
     }
