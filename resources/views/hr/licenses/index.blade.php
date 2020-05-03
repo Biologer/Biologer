8f267b1ace230c7c639e6ae906e57f86b1cee5b0
Nenad Živanović <nen.zivanovic@gmail.com>
2020-05-03 17:10:46 +0200

    Update licenses list page

--- resources/views/hr/licenses/index.blade.php
+++ resources/views/hr/licenses/index.blade.php
@@ -2,12 +2,28 @@
 
 @section('content')
     <div class="container">
-        <h1 class="title is-1 has-text-centered mt-4">Dostupne licence</h1>
+        <h1 class="title is-size-1 has-text-centered mt-4">Dostupne licence</h1>
 
-        @foreach (\App\License::allActive() as $license)
-            <p>
-                <a href="{{ $license->link }}">{{ $license->name() }}</a>
-            </p>
-        @endforeach
+        <div class="columns">
+            <div class="column">
+                <h2 class="is-size-3 mb-2">Licence podataka</h2>
+
+                @foreach (\App\License::allActive() as $license)
+                    <p>
+                        <a href="{{ $license->link }}">{{ $license->name() }}</a>
+                    </p>
+                @endforeach
+            </div>
+
+            <div class="column">
+                <h2 class="is-size-3 mb-2">Licence fotografija</h2>
+
+                @foreach (\App\License::allActive() as $license)
+                    <p>
+                        <a href="{{ $license->link }}">{{ $license->name() }}</a>
+                    </p>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/sr/licenses/index.blade.php
+++ resources/views/sr/licenses/index.blade.php
@@ -2,12 +2,28 @@
 
 @section('content')
     <div class="container">
-        <h1 class="title is-1 has-text-centered mt-4">Dostupne licence</h1>
+        <h1 class="title is-size-1 has-text-centered mt-4">Доступне лиценце</h1>
 
-        @foreach (\App\License::allActive() as $license)
-            <div>
-                <a href="{{ $license->link }}">{{ $license->name() }}</a>
+        <div class="columns">
+            <div class="column">
+                <h2 class="is-size-3 mb-2">Лиценце података</h2>
+
+                @foreach (\App\License::allActive() as $license)
+                    <p>
+                        <a href="{{ $license->link }}">{{ $license->name() }}</a>
+                    </p>
+                @endforeach
             </div>
-        @endforeach
+
+            <div class="column">
+                <h2 class="is-size-3 mb-2">Лиценце фотографија</h2>
+
+                @foreach (\App\License::allActive() as $license)
+                    <p>
+                        <a href="{{ $license->link }}">{{ $license->name() }}</a>
+                    </p>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
