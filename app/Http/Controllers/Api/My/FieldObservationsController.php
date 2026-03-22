86b2f30c802b68f1d890bc53c423b693a4ada3e0
Милош Поповић <gpopac@gmail.com>
2026-03-22 01:30:57 +0100

    Allow API to download my recent observations first

--- app/Http/Controllers/Api/My/FieldObservationsController.php
+++ app/Http/Controllers/Api/My/FieldObservationsController.php
@@ -11,15 +11,33 @@ class FieldObservationsController
     /**
      * Get field observations made by the user.
      *
+     * Available query parameters:
+     * @param string $order_by (Default: 'id')
+     * @param string $direction (Default: 'desc')
+     * @param int $per_page (Default: 15)
+     * @param string $updated_after (UNIX timestamp in secounds) 
+     * 
      * @param  \Illuminate\Http\Request  $request
      * @return \App\Http\Resources\FieldObservationResource
      */
     public function index(Request $request)
     {
-        $result = FieldObservation::createdBy($request->user())->isFieldObservation()->with([
-            'observation.taxon', 'observation.photos', 'activity.causer',
-            'observation.types.translations', 'observedBy', 'identifiedBy',
-        ])->filter($request)->isFieldObservation()->orderBy('id')->paginate($request->get('per_page', 15));
+        $result = FieldObservation::createdBy($request->user())
+            ->isFieldObservation()
+            ->with([
+                'observation.taxon',
+                'observation.photos',
+                'activity.causer',
+                'observation.types.translations',
+                'observedBy',
+                'identifiedBy',
+            ])
+            ->filter($request)
+            ->orderBy(
+                $request->get('order_by', 'id'),
+                $request->get('direction', 'desc')
+            )
+            ->paginate($request->get('per_page', 15));
 
         return FieldObservationResource::collection($result);
     }
